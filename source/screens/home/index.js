986e7591bbc85a3654ed29012c403fadfdada5aa
uma <uma@tigeen.ch>
2024-09-13 11:30:11 +0530

    dogs arranged

--- source/screens/home/index.js
+++ source/screens/home/index.js
@@ -1,13 +1,18 @@
-import {View} from 'react-native';
+import {Image, View} from 'react-native';
 import Header from './Header';
 import {styles} from './style';
 import Content from './Content';
+import Dog1 from '../../assets/dog1.png';
+import Dog2 from '../../assets/dog2.png';
+import Dog3 from '../../assets/dog3.png';
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Header />
-      {/* <Image /> */}
+      <Image source={Dog1} style={styles.dog1} />
+      <Image source={Dog2} style={styles.dog2} />
+      <Image source={Dog3} style={styles.dog3} />
       <Content />
     </View>
   );
--- source/screens/home/style.js
+++ source/screens/home/style.js
@@ -19,4 +19,26 @@ export const styles = StyleSheet.create({
     top: 218,
     flex: 1,
   },
+  dog1: {
+    width: 63.45,
+    height: 83.06,
+    top: 147.56,
+    left: 179,
+    position: 'absolute',
+  },
+  dog2: {
+    width: 75.73,
+    height: 90.41,
+    top: 142,
+    left: 237.45,
+    position: 'absolute',
+  },
+  dog3: {
+    width: 64.4,
+    height: 75.55,
+    top: 153.39,
+    left: 308.18,
+
+    position: 'absolute',
+  },
 });
