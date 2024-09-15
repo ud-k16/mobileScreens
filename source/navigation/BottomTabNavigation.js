c891f9500932d2d87e33e4ae8aa6b7cb57f55060
uma <uma@tigeen.ch>
2024-09-15 17:43:19 +0530

    bar icons added

--- source/navigation/BottomTabNavigation.js
+++ source/navigation/BottomTabNavigation.js
@@ -1,12 +1,23 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Explore from '../screens/explore';
 import Home from '../screens/home';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: () => {
+          if (route.name === 'Home') {
+            return <Entypo name="home" size={24} color="black" />;
+          } else if (route.name === 'Explore') {
+            return <Entypo name="shop" size={24} color="black" />;
+          }
+        },
+      })}>
       <Tab.Screen name="Explore" component={Explore} />
       <Tab.Screen name="Home" component={Home} />
     </Tab.Navigator>
